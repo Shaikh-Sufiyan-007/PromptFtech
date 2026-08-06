6a25283f0ca109190279ca6ba32b7bca6c681b3d
Shaikh-Sufiyan-007 <sufiyanali1705@gmail.com>
2026-08-06 19:23:21 +0530

    Error fixing part-2

--- backend/server.js
+++ backend/server.js
@@ -40,3 +40,5 @@ const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //   console.log(`Single-Admin Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 // });
+
+module.exports = app;
